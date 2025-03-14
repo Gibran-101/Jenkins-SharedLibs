d6f40c7d4c162e94f6d1b6fbc76bed7d26fdd558
Gibran Fahad <83395901+Gibran-101@users.noreply.github.com>
2025-03-14 17:47:09 -0500

    Update ecr_push.groovy
    Adding the repo name to the commands

--- vars/ecr_push.groovy
+++ vars/ecr_push.groovy
@@ -1,10 +1,9 @@
-// File: vars/ecr_push.groovy
-def call(String projectName, String imageTag, String awsRegion, String ecrRepositoryUri) {
+def call(String projectName, String imageTag, String awsRegion, String ecrRepositoryUri, String dockerHubUser) {
   // Authenticate with AWS ECR
   sh "aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${ecrRepositoryUri}"
   
-  // Tag the image for ECR
-  sh "docker tag ${projectName}:${imageTag} ${ecrRepositoryUri}/${projectName}:${imageTag}"
+  // Tag the image for ECR (accounting for the DockerHub username in the source image)
+  sh "docker tag ${dockerHubUser}/${projectName}:${imageTag} ${ecrRepositoryUri}/${projectName}:${imageTag}"
   
   // Push the image to ECR
   sh "docker push ${ecrRepositoryUri}/${projectName}:${imageTag}"
