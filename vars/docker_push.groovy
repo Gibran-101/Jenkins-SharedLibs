b126108c3c2a2b541393348bee841c2a26577acf
Gibran Fahad <gibranfahad1343787@gmail.com>
2025-06-26 11:06:40 -0500

    fix: docker hub credentials

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,9 @@
-def call(String Project, String ImageTag, String dockerhubuser){
-  withCredentials([usernamePassword(credentialsId: 'DHC', passwordVariable: 'dDHP', usernameVariable: 'DHU')]) {
-      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+def call(String project, String imageTag) {
+  withCredentials([usernamePassword(credentialsId: 'DHC', usernameVariable: 'DHU', passwordVariable: 'DHP')]) {
+    sh """
+      echo "\$DOCKERHUB_PASS" | docker login -u "\$DOCKERHUB_USER" --password-stdin
+      docker tag ${project}:${imageTag} \$DOCKERHUB_USER/${project}:${imageTag}
+      docker push \$DOCKERHUB_USER/${project}:${imageTag}
+    """
   }
-  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
