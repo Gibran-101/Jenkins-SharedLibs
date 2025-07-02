8e2f7fd437cbe1fc225a51898dfd72e599e2140a
Gibran Fahad <gibranfahad1343787@gmail.com>
2025-07-02 11:34:23 -0500

    fix: the shell commands were unable to run in Jenkins

--- vars/trivy_fs.groovy
+++ vars/trivy_fs.groovy
@@ -1,19 +1,4 @@
 def call() {
     echo "Running Trivy scan on file system..."
-
-    sh '''
-    if ! command -v trivy >/dev/null 2>&1; then
-        echo "Installing Trivy..."
-        sudo apt-get update
-        sudo apt-get install -y wget curl
-
-        TRIVY_VERSION=$(curl -s https://api.github.com/repos/aquasecurity/trivy/releases/latest | grep tag_name | cut -d '"' -f 4)
-        TRIVY_DEB="trivy_${TRIVY_VERSION#v}_Linux-64bit.deb"
-
-        wget https://github.com/aquasecurity/trivy/releases/download/$TRIVY_VERSION/$TRIVY_DEB -O trivy.deb
-        sudo dpkg -i trivy.deb
-    fi
-
-    trivy fs .
-    '''
+    sh 'trivy fs .'
 }
